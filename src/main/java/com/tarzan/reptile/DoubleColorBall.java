3685d87191a69d3b2edfd7983deb1b0daac5e85f
tarzan <1334512682@qq.com>
2021-04-01 09:57:35 +0800

    双色球号码随机生成

---
+++ src/main/java/com/tarzan/reptile/DoubleColorBall.java
@@ -0,0 +1,74 @@
+package com.tarzan.reptile;
+
+
+import cn.hutool.core.util.RandomUtil;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+/**
+ * @Author tarzan
+ * @Date 2021/4/1 14:43
+ * @Description 模拟双色球，随机生成若干住号码
+ */
+public class DoubleColorBall {
+
+
+    /**
+     * 主方法
+     * */
+    public static void main(String[] args) {
+        getDoubleColorBallNumber(5);
+    }
+
+    /**
+     * 获取多注双色球号码
+     * */
+    public static void getDoubleColorBallNumber(int num){
+        System.out.println("随机生成"+num+"注双色球号码为：");
+        String resultNumber="";
+        for (int i = 0; i < num; i++) {
+            System.out.println("【"+(i+1)+"】 "+resultNumber+getDoubleColorBallNumber());
+        }
+
+    }
+
+    /**
+     * 获取单注双色球号码
+     * */
+    public static String getDoubleColorBallNumber(){
+        String resultNumber="";
+        for (int i = 0; i < 6; i++) {
+            String ballNumber= RandomUtil.randomEle(getRedBalls())+"\t";
+            resultNumber=resultNumber+ballNumber;
+        }
+        return resultNumber+RandomUtil.randomEle(getBlueBalls());
+    }
+
+    /**
+     * 获取红球球号集合
+     * */
+    public static List<String> getRedBalls(){
+        return getBalls(33);
+    }
+
+    /**
+     * 获取蓝球球号集合
+     * */
+    public static List<String> getBlueBalls(){
+        return getBalls(16);
+    }
+
+    /**
+     * 获取球号集合
+     * */
+    public static List<String> getBalls(int num){
+        List<String> redBalls= Lists.newArrayList();
+        for (int i = 1; i <=num; i++) {
+            int length=String.valueOf(num).length();
+            String str = String.format("%0"+length+"d",i);
+            redBalls.add(str);
+        }
+        return redBalls;
+    }
+}
--- src/main/java/com/tarzan/reptile/demo/BlogComment.java
+++ src/main/java/com/tarzan/reptile/demo/BlogComment.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
@@ -28,7 +27,7 @@ public class BlogComment{
     private static String loginUrl = "https://passport.csdn.net/login";
 
     //获取文章列表接口
-    private static String moreBlogsApi = "https://blog.csdn.net/api/articles?type=more&category=db&shown_offset=0";
+    private static String moreBlogsApi = "https://blog.csdn.net/api/articles?type=more&category=ops&shown_offset=0";
 
     //评论接口
     private static String commentBlogApi = "https://blog.csdn.net/phoenix/web/v1/comment/submit";
@@ -54,7 +53,6 @@ public class BlogComment{
 
         commentList.add("话不多说，我先赞为敬！！！");
         commentList.add("学会了，感谢大佬分享，继续努力！");
-        commentList.add("赞赞赞，你真是个小天才！！");
         commentList.add("我一般不评论的，看到这篇文章了，我忍不住想对你说，真的写的太赞了。");
         commentList.add("学习了，感谢您的分享，让我受益良多！");
         commentList.add("谢谢分享，大佬的文章让我受益颇多！");
@@ -72,22 +70,25 @@ public class BlogComment{
         commentList.add("帮助很大，拒绝白嫖，点赞评论留个在此一游");
         commentList.add("代码之路任重道远，愿跟博主努力习之。");
         commentList.add("干货满满，很详细，评论占个坑。欢迎一起交流!");
-        commentList.add("在最美的年华，做最好的自己，加油！");
         commentList.add("我真是服了，像你这种人就是欠赞和关注，哼~");
         commentList.add("看完大佬的文章，我的心情竟是久久不能平静。正如老子所云：大音希声，大象无形。我现在终于明白我缺乏的是什么了。");
         commentList.add("由浅入深，适合有基础的技术人员。大佬可否给小弟回个赞感谢万分");
         commentList.add("君之妙笔，令鄙不及，佩服佩服 ，如若能给小弟回个赞，必将感激涕尽,哇，好棒啊，崇拜的小眼神，欢迎回赞，回评哦~~~");
         commentList.add("满满的干货，我嗅到的知识的芬芳！");
         commentList.add("写的挺不错的，继续加油哦！ 最近我也在学习写博客,有空来看看我呀，一起互相学习。期待你的关注与支持");
-        commentList.add("这篇文章对我很有帮助！作为一名java开发程序员，想和你一起交流自己的困惑和见解，可以到我主页留言私信一起交流吧！");
         commentList.add("给大佬递茶，最近也在学习相关知识，希望得到大佬的肯定和支持!");
         commentList.add("创作不易，给你打气,继续创作优质好文！");
+        commentList.add("博主不光能写的一手好代码，还能写的一手好文章。");
+        commentList.add("宋江、卢俊义、吴用、公孙胜、关胜、林冲、秦明、呼延灼、花荣、柴进、李应、朱仝、鲁智深、武松、董平、张清、杨志、徐宁、索超、戴宗、刘唐、李逵、史进、穆弘、雷横、李俊、阮小二、张横、阮小五、张顺、阮小七、杨雄、石秀、解珍、解宝、燕青、朱武、黄信、孙立、宣赞、郝思文、韩滔、彭玘、单廷珪、魏定国、萧让、裴宣、欧鹏、邓飞、燕顺、杨林、凌振、蒋敬、吕方、郭盛、安道全、皇甫端、王英、扈三娘、鲍旭、樊瑞、孔明、孔亮、项充、李衮、金大坚、马麟、童威、童猛、孟康、侯健、陈达、杨春、郑天寿、陶宗旺、宋清、乐和、龚旺、丁得孙、穆春、曹正、宋万、杜迁、薛永、施恩、周通、李忠、杜兴、汤隆、邹渊、邹润、朱富、朱贵、蔡福、蔡庆、李立、李云、焦挺、石勇、孙新、顾大嫂、张青、孙二娘、王定六、郁保四、白胜、时迁、段景柱等108人感觉很赞");
+        commentList.add("佐菲奥特曼、初代奥特曼、赛文奥特曼、杰克奥特曼、艾斯奥特曼、泰罗奥特曼、雷欧奥特曼、爱迪奥特曼、阿斯特拉奥特曼、尤迪安奥特曼、博伊奥特曼、乔尼亚斯奥特曼、史考特奥特曼、察克奥特曼、贝斯奥特曼、葛雷奥特曼、帕瓦特奥特曼、哉阿斯奥特曼、迪迦奥特曼、戴拿奥特曼、盖亚奥特曼、阿古茹奥特曼、高斯奥特曼、杰斯提斯奥特曼、雷杰多奥特曼、奈克斯特奥特曼、奈克瑟斯奥特曼、诺亚奥特曼、雷欧斯奥特曼、赛文奥特曼、麦克斯奥特曼、杰诺奥特曼、梦比优斯奥特曼、希卡利奥特曼、赛文X奥特曼、赛罗奥特曼、银河奥特曼、奥特之父、奥特之母、奥特之王等40位奥特曼觉得很赞。");
     }
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)  {
         start();
     }
+
+
 
 
     public static void start() {
@@ -97,7 +98,7 @@ public class BlogComment{
                 // ChromeOptions
                 ChromeOptions chromeOptions = new ChromeOptions();
                 // 设置后台静默模式启动浏览器
-                chromeOptions.addArguments("--headless");
+             //   chromeOptions.addArguments("--headless");
                 //启动浏览器
                 driver = new ChromeDriver(chromeOptions);
                 //登录
@@ -182,7 +183,7 @@ public class BlogComment{
             JSONObject resultJson=JSONObject.parseObject(result);
             if(resultJson.getInteger("code")!=200){
                //System.exit(1);
-                Thread.sleep(30*60*1000);//休息半个小时
+                Thread.sleep(60*60*1000);//休息1个小时
             }
             Thread.sleep(45000);//10秒评论一次
             } catch (InterruptedException interruptedException) {
@@ -257,6 +258,8 @@ public class BlogComment{
         });
 
     }
+
+
 
 
 }
